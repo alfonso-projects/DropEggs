fbe071ba38f96a380cd94dcc06228ea4f9ae6c13
powermilk <alftpa@gmail.com>
2019-01-25 22:50:10 +0100

    Fixed bugs and minors corrections

--- Egg.py
+++ Egg.py
@@ -1,3 +1,24 @@
+'''
+
+EXPLANATION:
+
+        we pass to the function the floor 0 and the total high of the skycrapper. We throw an egg from the middle of the skrycapper if the egg breaks we discart 
+        the floor from it has fallen to the finish of the srycapper, and we anotate that we have used another egg.
+
+        if has not been broken we bounded the building from 0 to to the floor that we have fallen the egg. 
+
+        We repeat this operation until we have only twoo floors or one. When we arrive to this situation we drop an egg from the middle flor, if the eggs broke, the critical floor is
+        the floor that we have dropped it minus one.
+
+
+        In the case that we only have one egg left to drop we can not use the avobe operation because we could waste the egg and not find the critical floor for that reason we will
+        start from the start index point and we will drop the egg from start + 2, if it breaks we know that the critical floor will be start -1, if is not broken, we update start 
+        to the last floor which we have dropped the egg and we will repeat the operation. Resolving the problem at this way will be more expensive but at the same time
+        we make sure that we won't waste the last egg that we have available
+        
+
+'''
+
 #we could create a class and encapsulate the functions and the variables inside of it.
 
 '''
@@ -6,11 +27,12 @@
         available_eggs = 100
         high = 100
         
-    medium dificulty:
+    Medium dificulty:
 
         available_eggs = 2
         high = 100
-    hard dificulty:
+        
+    Hard dificulty:
 
         high = the quantity that you want
         available_eggs = the quantity that you want
@@ -19,36 +41,15 @@
 
 #MODIFY VARIABLES HERE. GLOBAL VARIABLES
 
-available_eggs = 100
-high = 40
-critical_floor = 10
+available_eggs = 1
+high = 100
+critical_floor = 99
 
 
-
-'''
-
-EXLANATION:
-
-        we pass to the function the start and the finish of the skycrapper. We throw an egg from the middle of the skrycapper if the egg breaks we discart 
-        the floor from it has fallen to the finish of the skrycapper, and we anotate that we have used another egg.
-
-        if has not been broken we bounded the building from 0 to to the floor that we have fallen the egg. 
-
-        We repeat this operation until we have only twoo floors or one. When we arrive to this situation we drop an egg from the middle flor, if the eggs broke the critical floor is
-        the floor that we have dropped it minus one.
-
-
-        In the case that we only have one egg left for drop we can not use the avobe operation because we could waste the egg and not find the critical floor for that reason we will
-        start from the start index point and we will drop the egg from start + 2, if it breaks we know that the critical floor will be start -1, if is not broken, we update start 
-        to the last floor that we have dropped the egg and we will repeat the operation. Resolving the problem at this way will be more expensive but at the same time
-        we make sure that we wont waste the last egg that we have available
-        
-
-'''
 
 
 #______________________________________________
-#basic medium and hard difficulty
+# basic medium and hard difficulty
 # we pass to this funcion the number of available eggs and the high of the skycrapper and returns the numbers of tries to find the critical floor
 #______________________________________________
 
@@ -65,7 +66,6 @@ def minEggDropper100(eggs , high):
             if finish - start == 2 or finish-start == 1:
 
                 if(aux_egg_drop(middle) == False):
-                    #critical_floor_searched = start
                     eggs_used = eggs_used + 1
                     tries = tries +1
                     return tries
@@ -73,21 +73,19 @@ def minEggDropper100(eggs , high):
                     tries = tries +1
                     
                 if(aux_egg_drop(finish)== False):
-                    #critical_floor_searched = middle
                     tries = tries +1
                     eggs_used = eggs_used + 1
                 else:
                     tries = tries + 1
-                    critical_floor_searched = middle
                     
                 return tries
 
-            if aux_egg_drop(middle) == False: # si el medio es mayor que la planta critica entonces lo hemos jodido
+            if aux_egg_drop(middle) == False: 
                 eggs_used = eggs_used +1
                 finish = middle
                 tries = tries+1
                 res = recursive_function(start, finish, eggs_used, tries)
-            else: # sino miramos desde inicio hasta final
+            else: 
                 start = middle
                 tries = tries+1
                 res = recursive_function(start, finish, eggs_used, tries)
@@ -96,20 +94,15 @@ def minEggDropper100(eggs , high):
 
     def dropEgssCarefully(start, finish, eggs_used, tries):
 
-        critical_floor_searched = None
         while start <  finish:
             if aux_egg_drop(start) == False:
                 tries = tries +1
                 eggs_used = eggs_used +1
-                critical_floor_searched = start -1
                 break
             else:
                 tries=tries+1
                 start = start + 2
-
-        if finish == start and critical_floor_searched is None:
-            critical_floor_searched = finish
-
+    
         return tries
 
     eggs_used = 0
@@ -122,7 +115,7 @@ def minEggDropper100(eggs , high):
 #Auxiliar function. Drop an egg from a floor and returns true if the egss has broken and false if its fine and we can reuse the egg for the next try
 #______________________________________________
 
-def aux_egg_drop(floor): # true si no se ha roto, false si se ha roto
+def aux_egg_drop(floor): 
 
     if(floor > critical_floor):
         return False
